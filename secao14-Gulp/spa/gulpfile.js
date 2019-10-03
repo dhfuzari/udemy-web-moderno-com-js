0b653e4366a3f677eebcde753858bb152d23d6a5
Daniel H. Fuzari <dhfuzari@gmail.com>
2019-10-03 08:02:54 -0300

    feat(secao14): chapter gulp - part A

---
+++ secao14-Gulp/spa/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const util = require('gulp-util');
+const sequence = require('run-sequence');
+
+require('./gulpTasks/app');
+require('./gulpTasks/deps');
+require('./gulpTasks/server');
+
+gulp.task('default', () => {
+  if (util.env.production) {
+    // sequence('deps', 'app'); // An old way to run tasks sequencially
+    gulp.start('deps', 'app');
+  } else {
+    // sequence('deps', 'app', 'server'); // An old way to run tasks sequencially
+    gulp.start('deps', 'app', 'server');
+  }
+})
+

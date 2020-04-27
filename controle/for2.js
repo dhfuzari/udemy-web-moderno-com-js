3d7ee926ea245ab6284b3a2d37b62db326e34921
Daniel H. Fuzari <dhfuzari@gmail.com>
2020-04-27 08:52:06 -0300

    feat(controle): exercicios campitulo estruturas de controle

--- controle/for2.js
+++ controle/for2.js
@@ -1,16 +1,18 @@
-const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
 
+// For in para arrays: Devolve o indice atual do array para cada iteração
 for (let i in notas) {
-    console.log(i, notas[i])
+  console.log(i, ' -> ', notas[i]);
 }
 
+// For in para objetos: Devolve o atributo atual atual do objeto para cada iteração
 const pessoa = {
-    nome: 'Ana',
-    sobrenome: 'Silva',
-    idade: 29,
-    peso: 64
-}
+  nome: 'Ana',
+  sobrenome: 'Silva',
+  idade: 29,
+  peso: 64,
+};
 
-for(let atributo in pessoa) {
-    console.log(`${atributo} = ${pessoa[atributo]}`)
+for (let atributo in pessoa) {
+  console.log(`${atributo} -> ${pessoa[atributo]}`);
 }
